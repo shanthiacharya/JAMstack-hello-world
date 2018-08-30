e644a8d43e92f8c13b21282f0ffa4c30b31e586a
shanthiacharya <ShanthiAcharya@osxlyaumbp15.corp.elmae>
2018-08-30 12:59:29 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am Shanthi Acharya</p>
+    <p>I am passionate about Front end coding and want to develop innovative and creative applications.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
